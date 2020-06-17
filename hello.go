960b8f00fc8f9d60cfebbc7dffc3f3fb6f59fa86
David GILLARD <david.gillard@ac-lille.fr>
2020-06-17 20:04:30 +0200

    Mon Hello World en Go

---
+++ hello.go
@@ -0,0 +1,17 @@
+/*fichier : hello.
+auteur : David GILLARD */
+
+/*
+ce programe est le fameux hello world en go
+*/
+
+package main
+
+import "fmt"
+
+/* Un commentaire sur
+   plusieurs lignes */
+func main() {
+	// Un commentaire sur une ligne
+	fmt.Printf("Bonjour, tout le monde!\n")
+}
---
+++ playground.go
@@ -0,0 +1,20 @@
+/*fichier : playground.go
+auteur : David GILLARD */
+
+/*
+ce programe est le fameux hello world un peu amélioré en go
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+	fmt.Println("доброго вечера тебе")
+	fmt.Println("こんばんは")
+	fmt.Println("晚上好")
+
+}
